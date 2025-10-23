b667b749feeea6e08f8268ce0fcb32ab25d07e53
Rahul Khanna <30380069+photon6@users.noreply.github.com>
2025-10-23 12:22:16 -0400

    Delete custom_addons directory

--- custom_addons/quasy/test/test_quasy.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "https://photon6-propensic-main-24768631.dev.odoo.com/odoo/quasy/anonymize"
-payload = {"query": "John Smith from New York wants to transfer $10,000 to his account."}
-headers = {"Content-Type": "application/json"}
-
-response = requests.post(url, json=payload, headers=headers)
-print(response.status_code)
-#print(response.json())
